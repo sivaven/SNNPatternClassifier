dd9afbd815c6416409b4d33bf7f0093b8fdf3cdc
sivaven <sivaven7@gmail.com>
2014-10-18 21:01:48 -0400

    3.01

--- startGA.sh
+++ startGA.sh
@@ -2,5 +2,5 @@
 export CLASSPATH="src:bin:lib/*"
 module load python-2.7
 module load OpenBLAS
-javac src/briansim/*.java src/classifier/*.java src/dataset/*.java src/dataset/classes/*.java src/encode/*.java src/outputwriter/*.java src/snn/*.java src/snn/constants/*.java src/ecj/*.java src/utils/*.java
-nohup java training.ECJStarter &
+javac src/snn/*.java src/snn/constants/*.java src/briansim/*.java src/classifier/*.java src/dataset/*.java src/dataset/classes/*.java src/encode/*.java src/outputwriter/*.java src/ecj/*.java src/utils/*.java
+nohup java ecj.ECJStarter &
