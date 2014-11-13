281cd6ffb72dd369a305eaa36e2ff32e4363d86c
sivaven <sivaven7@gmail.com>
2014-11-13 13:20:36 -0500

    m/s

--- startGASlave.sh
+++ startGASlave.sh
@@ -3,4 +3,4 @@ export CLASSPATH="src:bin:lib/*"
 module load python-2.7
 module load OpenBLAS
 javac src/snn/*.java src/snn/constants/*.java src/briansim/*.java src/classifier/*.java src/dataset/*.java src/dataset/classes/*.java src/code/*.java src/outputwriter/*.java src/ecj/*.java src/utils/*.java
-nohup java ecj.ECJStarter2 --slave -file ecj_slave.params &
+nohup java ecj.ECJStarter2 --slave -file input/ecj_slave.params &
